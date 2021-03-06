6d6de9d0c77341bcfaa47d4e91f48610525f8de4
Diego <diegoandia7@gmail.com>
2021-03-06 23:52:38 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Doppelganger Prototype/Assets/Scripts/VFX/VFX_AttackAnimEvents.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFX_AttackAnimEvents : MonoBehaviour
+{
+    private void EndAttack()
+    {
+
+    }
+}
